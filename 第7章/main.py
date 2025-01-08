188043290160304fc29be03a278d169fa42dfede
Paul Harrison <19981014liu@gmail.com>
2025-01-08 21:29:12 +0800

    20250108

--- 第7章/main.py
+++ 第7章/main.py
@@ -2,14 +2,28 @@ import pygame
 import time
 from pygame.locals import *
 
-hero_x = 180
-hero_y = 500
+hero_x = 150
+hero_y = 600
 
 # 弹舱
 my_bullet = []
 
-enemy_x = 190
-enemy_path = 'left'
+# 加载飞机爆炸的多张图片
+a = pygame.image.load('./第7章/feiji/enemy2_down1.png')
+b = pygame.image.load('./第7章/feiji/enemy2_down2.png')
+c = pygame.image.load('./第7章/feiji/enemy2_down3.png')
+d = pygame.image.load('./第7章/feiji/enemy2_down4.png')
+e = pygame.image.load('./第7章/feiji/enemy2_down5.png')
+f = pygame.image.load('./第7章/feiji/enemy2_down6.png')
+
+# 定义一个列表
+blow_up = [a, b, c, d, e, f]
+
+enemy_life = 'live'
+
+enemy_x = 130
+enemy_path = 'right'
+enemy_num = 0 # 初始值为0, 代表爆炸的第一张图片, 循环时依次累加, 不超过4
 
 
 def hero_plane(screen, hero, bullet):
@@ -33,7 +47,7 @@ def hero_plane(screen, hero, bullet):
             elif event.key == K_RIGHT:
                 hero_x += 10
             elif event.key == K_SPACE:
-                my_bullet.append({'x': hero_x + 40, 'y': hero_y})
+                my_bullet.append({'x': hero_x + 32, 'y': hero_y-40})
 
     for i in my_bullet:
         screen.blit(bullet, (i['x'], i['y']))
@@ -42,19 +56,30 @@ def hero_plane(screen, hero, bullet):
 def enemy_plane(screen, enemy):
     global enemy_x
     global enemy_path
+    global enemy_life
+    global enemy_num
 
-    screen.blit(enemy, (enemy_x, 10))
+    for bullet in my_bullet:
+        if (bullet["x"] >= enemy_x and bullet["x"] <= enemy_x + 165) and (bullet["y"] >= 0 and bullet["y"] <= 265):
+            enemy_life = 'dead'
 
-    # 添加一个判断语句, 防止飞机越界
-    if enemy_x >= 411:
-        enemy_path = 'left'
-    elif enemy_x <= 0:
-        enemy_path = 'right'
+    if enemy_life == 'live':
+        screen.blit(enemy, (enemy_x, 10))
 
-    if enemy_path == 'left':
-        enemy_x -= 10
-    elif enemy_path == 'right':
-        enemy_x += 10
+        # 添加一个判断语句, 防止飞机越界
+        if enemy_x >= 250:
+            enemy_path = 'left'
+        elif enemy_x <= 0:
+            enemy_path = 'right'
+
+        if enemy_path == 'left':
+            enemy_x -= 10
+        elif enemy_path == 'right':
+            enemy_x += 10
+    elif enemy_life == 'dead':
+        if enemy_num <= 5:
+            screen.blit(blow_up[enemy_num], (enemy_x, 10))
+            enemy_num += 1
 
 
 
@@ -69,8 +94,7 @@ def main():
     # 创建一个bullet变量, 用于存放子弹图片信息
     bullet = pygame.image.load('./第7章/feiji/bullet.png')
     # 创建一个enemy变量, 用于存放敌机信息
-    enemy = pygame.image.load('./第7章/feiji/enemy1.png')
-
+    enemy = pygame.image.load('./第7章/feiji/enemy2.png')
 
 
     while True:
